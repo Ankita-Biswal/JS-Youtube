c2b2131c7e685699a485c2636951b9a7abf55cb8
Ankita Biswal <129891556+Ankita-Biswal@users.noreply.github.com>
2024-06-17 06:33:57 +0000

    conversion of datatypes

---
+++ 0-1Basics/0-3conversionOperation.js
@@ -0,0 +1,27 @@
+// let score = "Ankita"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number (score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber);
+/*notes*/
+//"33"=>33
+//"33abc"=>NaN
+//true=>1;false =>0;
+
+let isLoggedIn="Ankita";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn)
+/*notes*/
+//1=>true; 0=>false
+//""=>false
+//"ankita"=>true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber)
